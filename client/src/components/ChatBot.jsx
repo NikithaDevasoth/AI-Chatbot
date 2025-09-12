cd0209261e372ed6d5d514a115758d12deedc06f
NikithaDevasoth <devasothnikitha09@gmail.com>
2025-09-12 21:29:11 +0530

    Loading completed

--- client/src/components/ChatBot.jsx
+++ client/src/components/ChatBot.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { assets } from '../assets/assets';
+import Message from './Message';
 
 export const ChatBot = () => {
+  const { selectedChat, theme } = useAppContext();
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages)
+    }
+  }, [selectedChat])
+
   return (
-    <div>ChatBot</div>
+    <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
+      {/* Chat messages*/}
+      <div className='flex-1 mb-5 overflow-y-scroll'>
+        {messages.length === 0 && (
+          <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
+            <img
+              src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark}
+              alt=""
+              className='w-full max-w-56 sm:max-w-68'
+            />
+            <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white'>
+              Ask me anything.
+            </p>
+          </div>
+        )}
+
+        {messages.map((message, index) => (
+          <Message key={index} message={message} />
+        ))}
+
+        {/* Three Dots Loading Animation */}
+        {loading && (
+          <div className='loader flex items-center gap-1.5'>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'>
+            </div>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'>
+            </div>
+          </div>
+          
+        )}
+      </div>
+    </div>
   )
 }
